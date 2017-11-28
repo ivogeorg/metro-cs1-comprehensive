71d12b9ffd7373c04a0d4fa79655d04c21f696ac
Ivo Georgiev <ivogeorg@gmail.com>
2017-11-28 10:55:41 -0700

    typo in Cicle::toString

--- src/edu/msud/cs/cs1/Circle.java
+++ src/edu/msud/cs/cs1/Circle.java
@@ -2,13 +2,11 @@ package edu.msud.cs.cs1;
 
 public class Circle extends Elipsoid {
 
-    Circle(double x, double y, double r) {
-        super(x, y, r, r);
-    }
+    Circle(double x, double y, double r) { super(x, y, r, r); }
 
     @Override
     public String toString() {
-        return String.format("Cirle: x=%f y=%f r=%f", x, y, semiMajor);
+        return String.format("Circle: x=%f y=%f r=%f", x, y, semiMajor);
     }
 
 }
